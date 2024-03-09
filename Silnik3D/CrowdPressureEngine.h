5ccc8414f36ba7d59109aa8b3f679608e3077eaa
bart_cho <dsquer888@gmail.com>
2024-03-09 12:04:46 +0100

    Files from lab classes

---
+++ Silnik3D/CrowdPressureEngine.h
@@ -0,0 +1,14 @@
+#include"Types.h"
+#include <vector>
+
+class CrowdPressureEngine {
+
+
+public:
+
+	virtual const std::vector<const Types::Agent&> &getAgents() const = 0;
+	virtual const Types::Map& getMap() const = 0;
+
+	virtual void initialize(const Types::Configuration &configuration) = 0;
+	
+};
---
+++ Silnik3D/Types.h
@@ -0,0 +1,40 @@
+namespace Types {
+
+	class Agent {
+
+	private:
+
+		// todo: define arguments
+
+	public:
+
+		// todo: define methods
+
+	};
+
+	class Map {
+
+	private:
+
+		// todo: define arguments
+
+	public:
+
+		// todo: define methods
+
+	};
+
+	class Configuration {
+
+	private:
+
+		// todo: define arguments
+
+	public:
+
+		// todo: define methods
+		Map toMap() const {}
+
+	};
+
+}
