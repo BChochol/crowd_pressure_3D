5d4a4a7af2c3d723020d26148923e6025bf73ba8
bart_cho <dsquer888@gmail.com>
2024-03-09 12:01:06 +0100

    Template files

---
+++ Silnik3D/konfiguracja.cpp
@@ -0,0 +1,35 @@
+/*
+
+ C++ przez OpenGL - szablon do æwiczeñ laboratoryjnych
+ (C) Micha³ Turek.
+
+*/
+
+#ifdef _KONFIGURACJA
+
+
+	/******************* SZABLON **************************/
+
+
+
+
+
+
+
+	// KONFIGURACJA	
+
+	
+	ustalObszar (70);						// promien obszaru (kola), po jakim kamera mo¿e sie poruszaæ
+
+//	rejestrujPrzeszkode(-25,-25,-35,-35);	// Funkcja rejestruj¹ca przeszkody - kolejne parametry to X1, Z1, X2, Z2 prostok¹ta, który jest dodatkowo zabroniony dla kamery
+											// Funkcjê mo¿na wywo³ywac wielokrotnie dla ró¿nych obszarów
+
+
+
+
+
+	/******************************************************/
+
+
+#undef _KONFIGURACJA
+#endif
