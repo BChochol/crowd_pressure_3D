988201a938502cc12100556be27fb565b6c68939
shroobi <bartosz.chochol27@gmail.com>
2024-05-03 19:08:52 +0200

    Added json deserialization

--- CrowdControl3D/Assets/src/scripts/JsonSerialization.cs
+++ CrowdControl3D/Assets/src/scripts/JsonSerialization.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using Newtonsoft.Json;
 
 public class JsonSerialization : MonoBehaviour
 {
@@ -103,7 +106,42 @@ public class JsonSerialization : MonoBehaviour
     {
         Simulation simulation = new();
         
-        Dictionary<string, object> dict = JsonUtility.FromJson<Dictionary<string, object>>(json);
+        Debug.Log(json);
+        
+        Dictionary<string, object> dict = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+        
+        //foreach (KeyValuePair<string, object> pair in dict)
+        //{
+            Dictionary<string, object> responseDict = JsonConvert.DeserializeObject<Dictionary<string, object>>(dict["response"].ToString());
+            Dictionary<string, object> bodyDict = JsonConvert.DeserializeObject<Dictionary<string, object>>(responseDict["body"].ToString());
+            Dictionary<string, object> boardDict = JsonConvert.DeserializeObject<Dictionary<string, object>>(bodyDict["board"].ToString());
+            
+
+            simulation.simulationWidth = int.Parse(boardDict["width"].ToString());
+            simulation.simulationHeight = int.Parse(boardDict["height"].ToString());
+            
+            List<object> wallsDict = JsonConvert.DeserializeObject<List<object>>(boardDict["walls"].ToString(), new JsonSerializerSettings{ObjectCreationHandling = ObjectCreationHandling.Replace });
+            
+            List<Dictionary<string, Dictionary<string, float>>> walls = new();
+
+            foreach (object wall in wallsDict)
+            {
+                Dictionary<string, Dictionary<string, float>> newWall = new();
+                
+                Dictionary<string, object> wallDict =
+                    JsonConvert.DeserializeObject<Dictionary<string, object>>(wall.ToString());
+                
+                Dictionary<string, float> start =
+                    JsonConvert.DeserializeObject<Dictionary<string, float>>(wallDict["start"].ToString());
+                Dictionary<string, float> end =
+                    JsonConvert.DeserializeObject<Dictionary<string, float>>(wallDict["end"].ToString());
+                
+                newWall["start"] = start;
+                newWall["end"] = end;
+                walls.Append(newWall);
+            }
+        //}
+        
         return simulation;
     }
 }
